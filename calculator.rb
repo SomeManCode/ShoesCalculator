8c1fd354f6b5ff6fd6852e580a5e23455e1587d3
Joaquin Gamboa <gamboanj@gmail.com>
2013-03-26 19:11:53 -0400

    Implemented cases for button loop and added comments

--- calculator.rb
+++ calculator.rb
@@ -5,33 +5,30 @@ Shoes.app :title => "My Amazing Calculator", :width => 240, :height => 240 do
 
     #making buttons
     flow do
-      %w(7 8 9 + / 5 6 7 * - 1 2 3 0).each do |op|      
-        button op do         
-          append op
-        end
-      end
-      
-      button "C" do
-        clear_entry
-      end
-
-
-      button "=" do
-        eval_expression
-      end
-
+      %w(7 8 9 + / 5 6 7 * - 1 2 3 0 C =).each do |op|
+        #loop that goes through and checks each button value and calls appropriate message
+        button op do 
+          case op 
+            when "C"
+              clear_entry
+            when "="
+              eval_expression
+            else
+              append op
+           end  
+        end  
+      end  
     end
-
   end
   
-  # Stick a string on the end of our input
-  #
-
+  #clears entry
   def clear_entry()
     @input = ""
     @output.text = ""
   end
 
+
+  #Stick a string to the end of our input
   def append(s)
     if @input.nil?
       @input = ""
