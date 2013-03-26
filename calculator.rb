8825f4faa39c9de56e64080b4333a0647eb288b4
Joaquin Gamboa <gamboanj@gmail.com>
2013-03-26 19:45:40 -0400

    Square root added

--- calculator.rb
+++ calculator.rb
@@ -5,7 +5,7 @@ Shoes.app :title => "My Amazing Calculator", :width => 240, :height => 240 do
 
     #making buttons
     flow do
-      %w(7 8 9 + / 5 6 7 * - 1 2 3 0 C =).each do |op|
+      %w(7 8 9 + / 5 6 7 * - 1 2 3 0 C = ** sq).each do |op|
         #loop that goes through and checks each button value and calls appropriate message
         button op do 
           case op 
@@ -13,9 +13,14 @@ Shoes.app :title => "My Amazing Calculator", :width => 240, :height => 240 do
               clear_entry
             when "="
               eval_expression
+            when "sq"
+               square_root
+
+               # @input = (Math.sqrt(@input)
+               # @output.text = @input
             else
               append op
-           end  
+          end  
         end  
       end  
     end
@@ -44,4 +49,9 @@ Shoes.app :title => "My Amazing Calculator", :width => 240, :height => 240 do
     @output.text = @input
   end
   
+  def square_root
+    @input = Math.sqrt(@input.to_i).to_s
+    @output.text = @input
+  end
+   
 end
